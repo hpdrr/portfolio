b8014467116bc7913a1880c13c094f166562741d
Hapidr <hapidr24@gmail.com>
2023-02-03 22:25:43 +0700

    Hero section selesai

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#f97316",
         dark: "#1f2937",
         sky: "#0c4a6e",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },

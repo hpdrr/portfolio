58d8eb8c6f8c9904f9bb51bbb8e1b43ef908e594
Hapidr <hapidr24@gmail.com>
2023-02-03 21:26:52 +0700

    Membuat elemen hero section sebelah kiri(introduction)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f97316",
+        dark: "#1f2937",
+        sky: "#0c4a6e",
+      },
+    },
   },
   plugins: [],
 };

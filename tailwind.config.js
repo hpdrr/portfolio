5c57857cb665d4d34d52ab80dcc86d608347766b
Hapidr <hapidr24@gmail.com>
2023-02-04 13:30:22 +0700

    Membuat portfolio section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
     },
     extend: {
       colors: {
+        // orange
         primary: "#f97316",
+        // gray 800
         dark: "#1f2937",
+        // sky
         sky: "#0c4a6e",
       },
       screens: {

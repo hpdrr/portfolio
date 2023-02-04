3978f81082f5a00076c24cb20d8b07a64f49dc55
Hapidr <hapidr24@gmail.com>
2023-02-04 11:12:31 +0700

    membuat navbar menjadi responsif

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,7 +12,9 @@ window.onscroll = () => {
 
 // Hamburger
 const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
